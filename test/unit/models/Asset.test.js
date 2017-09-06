0c8a2e7e0e4f46b32c759ea8cdb38932c9262c2b
Richard Cooke <rcooke@nu-worx.com>
2017-09-06 08:03:34 +0100

    added mocha for testing

---
+++ test/unit/models/Asset.test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const assert = require('chai').assert;
+const Model = require('../../../api/models/Asset');
+const Fixture = require('../../fixtures/AssetFixture');
+
+describe('models:Asset', () => {
+  it('Should be tested', () => {
+    assert(false);
+  });
+});
